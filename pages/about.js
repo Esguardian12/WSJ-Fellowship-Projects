b7b436236d03dc99c6d9abcd9604d08ec454c089
Esguardian12 <45113081+Esguardian12@users.noreply.github.com>
2021-03-02 17:55:07 -0500

    Add files via upload

---
+++ pages/about.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+
+function About() {
+    return ( <div>
+        
+        <Head>
+            <script async defer data-website-id="7b539800-c9b9-4b4e-bf31-ae7d7ed58ed7" src="http://localhost:3000/umami.js"></script>
+                <title>My page title</title>
+        </Head>
+        <body>
+            <h1>ABOUT WebPage</h1>
+            <h2>About page details</h2>
+
+            <p>Hello World! This is the first attempt at detailing a webpage.</p>
+
+            <button>Click here to learn more</button>
+
+        </body>
+        
+        About</div>
+        )
+  }
+  
+  export default About
+
+
